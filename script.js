c3eda499aa857db4c582f133236df1e5dff3abeb
wandsen <dsenwan@gmail.com>
2018-11-11 16:26:15 +0800

    basic deliverables

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+
+class CreateListTag extends React.Component{
+    render(){
+        return(
+            <li>{this.props.listItem}</li>
+
+        );
+    }
+}
+
+
+
 class List extends React.Component {
   constructor(){
     super()
     this.changeHandler = this.changeHandler.bind( this );
+    this.addToList = this.addToList.bind( this );
+
   }
 
   state = {
@@ -14,14 +28,35 @@ class List extends React.Component {
     console.log("change", event.target.value);
   }
 
+  addToList(){
+    const newList = this.state.list.slice()
+    newList.push(this.state.word)
+
+    this.setState({list: newList })
+    this.setState({word: ""})
+  }
+
   render() {
       // render the list with a map() here
+
+      let toDoList = this.state.list.map( (element, index)=>{
+
+            return <CreateListTag key={index} listItem = {element} />
+      })
+
+      console.log(this.state.list)
 
       console.log("rendering");
       return (
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <button onClick = {this.addToList}>add item</button>
+
+          <ul>
+            {toDoList}
+          </ul>
+
+
         </div>
       );
   }
